c7ba61ada7ece18696a5c3f1a08edd53e52731a2
czt.pe <130927208+CarlosZevallosTrigoso@users.noreply.github.com>
2025-10-14 16:13:13 -0500

    Update Bayer.js

--- src/algorithms/OrderedDithering/Bayer.js
+++ src/algorithms/OrderedDithering/Bayer.js
@@ -7,21 +7,34 @@ class Bayer extends BaseAlgorithm {
 
   process(pixels, width, height, config, utils) {
     const { lumaLUT, bayerLUT } = utils;
-    const { colorCount, patternStrength } = config;
+    const { colorCount, patternStrength, useOriginalColor } = config;
 
-    const ditherStrength = (255 / colorCount) * patternStrength * 2;
+    const ditherStrength = (255 / (colorCount -1)) * patternStrength * 2;
+    const levels = Math.max(1, colorCount - 1);
 
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
         const i = (y * width + x) * 4;
         const ditherOffset = bayerLUT.get(x, y) * ditherStrength;
-        const luma = pixels[i] * 0.299 + pixels[i + 1] * 0.587 + pixels[i + 2] * 0.114;
-        const adjustedLuma = Math.min(255, Math.max(0, luma + ditherOffset));
-        const [r, g, b] = lumaLUT.map(adjustedLuma);
 
-        pixels[i] = r;
-        pixels[i + 1] = g;
-        pixels[i + 2] = b;
+        if (useOriginalColor) {
+          const r = pixels[i];
+          const g = pixels[i + 1];
+          const b = pixels[i + 2];
+
+          pixels[i] = Math.round((Math.min(255, Math.max(0, r + ditherOffset))) / 255 * levels) * (255 / levels);
+          pixels[i+1] = Math.round((Math.min(255, Math.max(0, g + ditherOffset))) / 255 * levels) * (255 / levels);
+          pixels[i+2] = Math.round((Math.min(255, Math.max(0, b + ditherOffset))) / 255 * levels) * (255 / levels);
+
+        } else {
+          const luma = pixels[i] * 0.299 + pixels[i + 1] * 0.587 + pixels[i + 2] * 0.114;
+          const adjustedLuma = Math.min(255, Math.max(0, luma + ditherOffset));
+          const [r, g, b] = lumaLUT.map(adjustedLuma);
+
+          pixels[i] = r;
+          pixels[i + 1] = g;
+          pixels[i + 2] = b;
+        }
       }
     }
     return pixels;
